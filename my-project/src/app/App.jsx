bc18aa089cb412554c2bba7e8e4aa35aec591a98
Gustavooxx <gustaviixx@gmail.com>
2025-07-31 16:52:06 -0300

    copia pronta

---
+++ my-project/src/app/App.jsx
@@ -0,0 +1,102 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './app.scss'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="conteinar-app">
+
+      <div className='titulo-app'>
+        <h1>PRECISA DE AJUDA? NÓS DAMOS TODO O SUPORTE.</h1>
+      </div>
+      
+      <div className='descricao_app'>
+        <p>De Configurações de conta de Permissões, encontre ajuda para tudo no Discord <br /> 
+        Se você é novo no Discord e está procurando por dicas, confira nosso Guia do Iniciante.</p>
+      </div>
+
+
+      <div className='conteinar-cartao'>
+
+          <div className='cartao'>
+            <img src="/public/images/imagem1.svg" alt="imagem1"  />
+            <div className='titulo-image'><h2>Anúncio</h2></div>
+            <div className='descricao-image'><p>Estamos atentos ao que acontece. <br /> Aqui está o que você precisa saber</p></div>
+          </div>
+
+          <div className='cartao'>
+            <img src="/public/images/02JYJB586GD8QZHGXWF7F76KVK.svg" alt="imagem1"  />
+            <div className='titulo-image'><h2>Anúncio</h2></div>
+            <div className='descricao-image'><p>Estamos atentos ao que acontece. Aqui está o que você precisa saber</p></div>
+          </div>
+
+          <div className='cartao'>
+            <img src="/public/images/03JYJB58795R54EC30KW0T81TT.svg" alt="imagem1"  />
+            <div className='titulo-image'><h2>Anúncio</h2></div>
+            <div className='descricao-image'><p>Estamos atentos ao que acontece. Aqui está o que você precisa saber</p></div>
+          </div>
+
+          <div className='cartao'>
+            <img src="/public/images/04JYJB56Y3VTBGNMRP49RYNV8G.svg" alt="imagem1"  />
+            <div className='titulo-image'><h2>Anúncio</h2></div>
+            <div className='descricao-image'><p>Estamos atentos ao que acontece. Aqui está o que você precisa saber</p></div>
+          </div>
+
+          <div className='cartao'>
+            <img src="/public/images/05JYJB57BF1WV546KFHXF5H38J.svg" alt="imagem1"  />
+            <div className='titulo-image'><h2>Anúncio</h2></div>
+            <div className='descricao-image'><p>Estamos atentos ao que acontece. Aqui está o que você precisa saber</p></div>
+          </div>
+
+          <div className='cartao'>
+            <img src="/public/images/06JYJB587CAW49MPKJHQ6A0X17.svg" alt="imagem1"  />
+            <div className='titulo-image'><h2>Anúncio</h2></div>
+            <div className='descricao-image'><p>Estamos atentos ao que acontece. Aqui está o que você precisa saber</p></div>
+          </div>
+
+          <div className='cartao'>
+            <img src="/public/images/07JYJB569J0Z7KA5YCE3FVAQY3.svg" alt="imagem1"  />
+            <div className='titulo-image'><h2>Anúncio</h2></div>
+            <div className='descricao-image'><p>Estamos atentos ao que acontece. Aqui está o que você precisa saber</p></div>
+          </div>
+
+          <div className='cartao'>
+            <img src="/public/images/08JYJB55H85D76GRQDPYXAE524.svg" alt="imagem1"  />
+            <div className='titulo-image'><h2>Anúncio</h2></div>
+            <div className='descricao-image'><p>Estamos atentos ao que acontece. Aqui está o que você precisa saber</p></div>
+          </div>
+
+          <div className='cartao'>
+            <img src="/public/images/09JYJB57AF63TSC5Q2TNA0NW5H.svg" alt="imagem1"  />
+            <div className='titulo-image'><h2>Anúncio</h2></div>
+            <div className='descricao-image'><p>Estamos atentos ao que acontece. Aqui está o que você precisa saber</p></div>
+          </div>
+
+          <div className='cartao'>
+            <img src="/public/images/010JYJB57EJ22HPHYMYJ4TJCN29.svg" alt="imagem1"  />
+            <div className='titulo-image'><h2>Anúncio</h2></div>
+            <div className='descricao-image'><p>Estamos atentos ao que acontece. Aqui está o que você precisa saber</p></div>
+          </div>
+
+          <div className='cartao'>
+            <img src="/public/images/011JYJB55FC8XWE4TRK66CXM33K.svg" alt="imagem1"  />
+            <div className='titulo-image'><h2>Anúncio</h2></div>
+            <div className='descricao-image'><p>Estamos atentos ao que acontece. Aqui está o que você precisa saber</p></div>
+          </div>
+
+          <div className='cartao'>
+            <img src="/public/images/012JYJB5678TM70ASBFF43GM556.svg" alt="imagem1"  />
+            <div className='titulo-image'><h2>Anúncio</h2></div>
+            <div className='descricao-image'><p>Estamos atentos ao que acontece. Aqui está o que você precisa saber</p></div>
+          </div>
+
+          
+
+      </div>
+    </div>
+  )
+}
+
+export default App
