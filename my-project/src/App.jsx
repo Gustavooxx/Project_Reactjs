f9ae00368408ca919092e5b2b48d87243282307d
Gustavooxx <gustaviixx@gmail.com>
2025-07-31 13:53:16 -0300

    inicio

---
+++ my-project/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './app.sass'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>Olá Mundo!!</h1>
+    </div>
+  )
+}
+
+export default App
