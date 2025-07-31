100663313444af35a659931beec1f9681c410435
Gustavooxx <gustaviixx@gmail.com>
2025-07-31 17:53:40 -0300

    pastas arrumadas

--- my-project/src/main.jsx
+++ my-project/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.scss'
-import App from './app/App.jsx'
+import App from './pages/app/index.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- my-project/src/pages/app/index.jsx
+++ my-project/src/pages/app/index.jsx
@@ -20,74 +20,74 @@ function App() {
 
       <div className='conteinar-cartao'>
 
-          <div className='cartao'>
-            <img src="/public/images/imagem1.svg" alt="imagem1"  />
+        <div className='cartao'>
+            <img src="/public/assets/images/imagem1.svg" alt="imagem1"  />
             <div className='titulo-image'><h2>Anúncio</h2></div>
             <div className='descricao-image'><p>Estamos atentos ao que acontece. <br /> Aqui está o que você precisa saber</p></div>
           </div>
 
           <div className='cartao'>
-            <img src="/public/images/02JYJB586GD8QZHGXWF7F76KVK.svg" alt="imagem1"  />
+            <img src="/public/assets/images/02JYJB586GD8QZHGXWF7F76KVK.svg" alt="imagem1"  />
             <div className='titulo-image'><h2>Anúncio</h2></div>
             <div className='descricao-image'><p>Estamos atentos ao que acontece. Aqui está o que você precisa saber</p></div>
           </div>
 
           <div className='cartao'>
-            <img src="/public/images/03JYJB58795R54EC30KW0T81TT.svg" alt="imagem1"  />
+            <img src="/public/assets/images/03JYJB58795R54EC30KW0T81TT.svg" alt="imagem1"  />
             <div className='titulo-image'><h2>Anúncio</h2></div>
             <div className='descricao-image'><p>Estamos atentos ao que acontece. Aqui está o que você precisa saber</p></div>
           </div>
 
           <div className='cartao'>
-            <img src="/public/images/04JYJB56Y3VTBGNMRP49RYNV8G.svg" alt="imagem1"  />
+            <img src="/public/assets/images/04JYJB56Y3VTBGNMRP49RYNV8G.svg" alt="imagem1"  />
             <div className='titulo-image'><h2>Anúncio</h2></div>
             <div className='descricao-image'><p>Estamos atentos ao que acontece. Aqui está o que você precisa saber</p></div>
           </div>
 
           <div className='cartao'>
-            <img src="/public/images/05JYJB57BF1WV546KFHXF5H38J.svg" alt="imagem1"  />
+            <img src="/public/assets/images/05JYJB57BF1WV546KFHXF5H38J.svg" alt="imagem1"  />
             <div className='titulo-image'><h2>Anúncio</h2></div>
             <div className='descricao-image'><p>Estamos atentos ao que acontece. Aqui está o que você precisa saber</p></div>
           </div>
 
           <div className='cartao'>
-            <img src="/public/images/06JYJB587CAW49MPKJHQ6A0X17.svg" alt="imagem1"  />
+            <img src="/public/assets/images/06JYJB587CAW49MPKJHQ6A0X17.svg" alt="imagem1"  />
             <div className='titulo-image'><h2>Anúncio</h2></div>
             <div className='descricao-image'><p>Estamos atentos ao que acontece. Aqui está o que você precisa saber</p></div>
           </div>
 
           <div className='cartao'>
-            <img src="/public/images/07JYJB569J0Z7KA5YCE3FVAQY3.svg" alt="imagem1"  />
+            <img src="/public/assets/images/07JYJB569J0Z7KA5YCE3FVAQY3.svg" alt="imagem1"  />
             <div className='titulo-image'><h2>Anúncio</h2></div>
             <div className='descricao-image'><p>Estamos atentos ao que acontece. Aqui está o que você precisa saber</p></div>
           </div>
 
           <div className='cartao'>
-            <img src="/public/images/08JYJB55H85D76GRQDPYXAE524.svg" alt="imagem1"  />
+            <img src="/public/assets/images/08JYJB55H85D76GRQDPYXAE524.svg" alt="imagem1"  />
             <div className='titulo-image'><h2>Anúncio</h2></div>
             <div className='descricao-image'><p>Estamos atentos ao que acontece. Aqui está o que você precisa saber</p></div>
           </div>
 
           <div className='cartao'>
-            <img src="/public/images/09JYJB57AF63TSC5Q2TNA0NW5H.svg" alt="imagem1"  />
+            <img src="/public/assets/images/09JYJB57AF63TSC5Q2TNA0NW5H.svg" alt="imagem1"  />
             <div className='titulo-image'><h2>Anúncio</h2></div>
             <div className='descricao-image'><p>Estamos atentos ao que acontece. Aqui está o que você precisa saber</p></div>
           </div>
 
           <div className='cartao'>
-            <img src="/public/images/010JYJB57EJ22HPHYMYJ4TJCN29.svg" alt="imagem1"  />
+            <img src="/public/assets/images/010JYJB57EJ22HPHYMYJ4TJCN29.svg" alt="imagem1"  />
             <div className='titulo-image'><h2>Anúncio</h2></div>
             <div className='descricao-image'><p>Estamos atentos ao que acontece. Aqui está o que você precisa saber</p></div>
           </div>
 
           <div className='cartao'>
-            <img src="/public/images/011JYJB55FC8XWE4TRK66CXM33K.svg" alt="imagem1"  />
+            <img src="/public/assets/images/011JYJB55FC8XWE4TRK66CXM33K.svg" alt="imagem1"  />
             <div className='titulo-image'><h2>Anúncio</h2></div>
             <div className='descricao-image'><p>Estamos atentos ao que acontece. Aqui está o que você precisa saber</p></div>
           </div>
 
           <div className='cartao'>
-            <img src="/public/images/012JYJB5678TM70ASBFF43GM556.svg" alt="imagem1"  />
+            <img src="/public/assets/images/012JYJB5678TM70ASBFF43GM556.svg" alt="imagem1"  />
             <div className='titulo-image'><h2>Anúncio</h2></div>
             <div className='descricao-image'><p>Estamos atentos ao que acontece. Aqui está o que você precisa saber</p></div>
           </div>
